5f3d98890fbbdb9c49cd5b14134fc834710c0619
Italo Sebastián Clavijo Curi <106178559+ItaloClavijo@users.noreply.github.com>
2024-06-11 16:53:26 -0500

    Commit #7 Creacion de Pagina Home para atender ruta base incluyendo servicio para recuperacion de imagenes desde el backend

---
+++ src/app/components/musictool/home/index/index.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { ShowContentService } from '../../../../services/show-content.service';
+import { Content } from '../../../../model/Content';
+import {MatCardModule} from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { ApiImgPipe } from '../../../../shared/api-img.pipe';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-index',
+  standalone: true,
+  imports: [RouterOutlet,
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule,
+    ApiImgPipe,
+    RouterModule,
+    CommonModule],
+  templateUrl: './index.component.html',
+  styleUrl: './index.component.css'
+})
+export class IndexComponent implements OnInit {
+
+  constructor(private showContent: ShowContentService){
+  }
+  contents?: Content[];
+
+  ngOnInit(): void {
+    this.showContent.getLastContents().subscribe((contents)=>{
+      this.contents=contents;
+    })
+  }
+
+}
