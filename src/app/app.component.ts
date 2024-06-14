f3825444266baafb00fe4fe6c7f5a54ab001323e
Italo Sebastián Clavijo Curi <106178559+ItaloClavijo@users.noreply.github.com>
2024-06-14 06:03:46 -0500

    Commit #18 Implementacion de Landing y mejora de login page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
@@ -6,6 +6,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
 import { NgIf } from '@angular/common';
 import { LoginService } from './services/login.service';
+import { UserService } from './services/user.service';
 
 @Component({
   selector: 'app-root',
@@ -23,15 +24,17 @@ import { LoginService } from './services/login.service';
 })
 export class AppComponent {
   title = 'MusicToool-FrontendTF';
+  username: string = ''
   role: string = '';
-  constructor(private loginService: LoginService) {}
-
+  constructor(private loginService: LoginService,
+  ) {}
   cerrar() {
     sessionStorage.clear();
   }
 
   verificar() {
     this.role = this.loginService.showRole();
+    this.username = this.loginService.showUser();
     return this.loginService.verificar();
   }
   isUser() {
@@ -41,4 +44,6 @@ export class AppComponent {
   isAdmin() {
     return this.role === 'ADMIN';
   }
+  
+
 }
