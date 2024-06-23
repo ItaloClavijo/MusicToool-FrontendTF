5503634516c55d0a3d0065e63dc0663c6b79799c
JLuisR04 <u202110143@upc.edu.pe>
2024-06-22 20:49:18 -0500

    Commit #26 se agrego roles

--- src/app/components/musictool/role/listrole/listrole.component.ts
+++ src/app/components/musictool/role/listrole/listrole.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { Roles } from '../../../../model/Role';
+import { RoleService } from '../../../../services/role.service';
 
 @Component({
   selector: 'app-listrole',
   standalone: true,
-  imports: [],
+  imports: [MatTableModule, MatPaginatorModule],
   templateUrl: './listrole.component.html',
   styleUrl: './listrole.component.css'
 })
-export class ListroleComponent {
+export class ListroleComponent implements OnInit{
+  dataSource: MatTableDataSource<Roles> = new MatTableDataSource();
+  displayedColumns: string[] = ['c1', 'c2', 'c3', 'c4'];
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
 
+  constructor(private rS:RoleService) { }
+
+  ngOnInit(): void {
+    this.rS.list().subscribe((data) => {
+      this.dataSource = new MatTableDataSource(data);
+      this.dataSource.paginator = this.paginator;
+    });
+    this.rS.getList().subscribe((data) => {
+      this.dataSource = new MatTableDataSource(data);
+      this.dataSource.paginator = this.paginator;
+    });
+  }
 }
